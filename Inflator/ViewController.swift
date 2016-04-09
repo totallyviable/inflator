a1029bb96e59dc68cdb980e60b29b370d643f025
jake <jacob.bijani@gmail.com>
2016-04-09 17:59:04 -0400

    Initial Commit

---
+++ Inflator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Inflator
+//
+//  Created by Jacob Bijani on 4/9/16.
+//  Copyright © 2016 Totally Viable. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
